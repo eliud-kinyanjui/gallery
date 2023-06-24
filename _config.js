9b1a3a0db71dd8cb4e3eec688133c9a0cf4aa236
Eliud Kinyanjui <eliud.kinyanjui@ramani.co.ke>
2023-06-24 15:01:23 +0300

    DB Config update

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://kinyanjuingugi:oX4idVIUDElHQIav@cluster0.yu8uvfz.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://kinyanjuingugi:oX4idVIUDElHQIav@cluster0.yu8uvfz.mongodb.net//darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://kinyanjuingugi:oX4idVIUDElHQIav@cluster0.yu8uvfz.mongodb.net//darkroom-test?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kinyanjuingugi:oX4idVIUDElHQIav@cluster0.yu8uvfz.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kinyanjuingugi:oX4idVIUDElHQIav@cluster0.yu8uvfz.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
